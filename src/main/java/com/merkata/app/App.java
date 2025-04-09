ab3fdafaa47060b99be3ef72878eacadd7ab754c
Mariyan Dimitrov <mariyan_dimitrov@flutterint.com>
2025-04-09 12:54:30 +0300

    feat: Change greeting

--- src/main/java/com/merkata/app/App.java
+++ src/main/java/com/merkata/app/App.java
@@ -5,6 +5,6 @@ package com.merkata.app;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Beautiful World!");
     }
 }
